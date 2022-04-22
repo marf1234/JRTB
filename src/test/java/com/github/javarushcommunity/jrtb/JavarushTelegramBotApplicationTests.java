2ae641594e0f333001a6a8eb18b4473fd1fbde30
Андрей <andre.veg@gmail.com>
2022-04-22 09:49:33 +0300

    JRTB-0.0

--- src/test/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplicationTests.java
+++ src/test/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplicationTests.java
@@ -3,6 +3,7 @@ package com.github.javarushcommunity.jrtb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class JavarushTelegramBotApplicationTests {
 
